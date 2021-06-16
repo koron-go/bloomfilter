d14df9c5cbf327a47c8557a52c61d0aca346be1f
MURAOKA Taro <koron.kaoriya@gmail.com>
2021-06-16 15:33:42 +0900

    support large size

---
+++ vbf3redis/vbf3_redis.go
@@ -0,0 +1,345 @@
+package vbf3redis
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/dgryski/go-metro"
+	"github.com/go-redis/redis/v8"
+)
+
+const redisWatchRetryMax = 5
+
+func watchWithRetry(ctx context.Context, uc redis.UniversalClient, fn func(tx *redis.Tx) error, keys ...string) error {
+	for retries := redisWatchRetryMax; retries > 0; retries-- {
+		err := uc.Watch(ctx, fn, keys...)
+		if err != nil && errors.Is(err, redis.TxFailedErr) {
+			continue
+		}
+		return err
+	}
+	return fmt.Errorf("transaction failed %d times: %w", 5, redis.TxFailedErr)
+}
+
+type keyBase string
+
+func (kb keyBase) data(n int) string {
+	return string(kb) + "_" + strconv.Itoa(n)
+}
+
+func (kb keyBase) props() string {
+	return string(kb) + "_props"
+}
+
+func (kb keyBase) gen() string {
+	return string(kb) + "_gen"
+}
+
+// VBF3Redis provides VBF3 with Redis backend.
+type VBF3Redis struct {
+	key keyBase
+
+	vbf3props
+
+	c redis.UniversalClient
+
+	pageNum int
+}
+
+// vbf3props codes constant properties of VBF3.
+type vbf3props struct {
+	M uint64 `json:"m"`
+	K uint   `json:"k"`
+
+	MaxLife uint8 `json:"max_life"`
+
+	SeedBase uint64 `json:"seed_base"` // for future use
+}
+
+func propsGet(ctx context.Context, c redis.Cmdable, key keyBase) (*vbf3props, bool, error) {
+	b, err := c.Get(ctx, key.props()).Bytes()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return nil, false, nil
+		}
+		return nil, false, fmt.Errorf("failed to get properties info with key %q: %w", key.props(), err)
+	}
+	var v *vbf3props
+	err = json.Unmarshal(b, &v)
+	if err != nil {
+		return nil, false, fmt.Errorf("invalid format of generation info: %w", err)
+	}
+	return v, true, nil
+}
+
+func propsPut(ctx context.Context, c redis.Cmdable, key keyBase, p *vbf3props) error {
+	b, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+	_, err = c.Set(ctx, key.props(), b, 0).Result()
+	if err != nil {
+		return fmt.Errorf("failed to put properties with key %q: %w", key.props(), err)
+	}
+	return nil
+}
+
+// vbf3gen codes generation parameters of VBF3.
+type vbf3gen struct {
+	Bottom uint8 `json:"bottom"`
+	Top    uint8 `json:"top"`
+}
+
+func getGen(ctx context.Context, c redis.Cmdable, key keyBase) (*vbf3gen, error) {
+	b, err := c.Get(ctx, key.gen()).Bytes()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get generation info with key %q: %w", key.gen(), err)
+	}
+	var v *vbf3gen
+	err = json.Unmarshal(b, &v)
+	if err != nil {
+		return nil, fmt.Errorf("invalid format of generation info: %w", err)
+	}
+	return v, nil
+}
+
+func putGen(ctx context.Context, c redis.Cmdable, key keyBase, g *vbf3gen) error {
+	b, err := json.Marshal(g)
+	if err != nil {
+		return err
+	}
+	_, err = c.Set(ctx, key.gen(), b, 0).Result()
+	if err != nil {
+		return fmt.Errorf("failed to put generation info with key %q: %w", key.gen(), err)
+	}
+	return nil
+}
+
+func (g *vbf3gen) isValid(n uint8) bool {
+	if n == 0 {
+		return false
+	}
+	a := g.Bottom <= n
+	b := g.Top >= n
+	if g.Bottom <= g.Top {
+		return a && b
+	}
+	return a || b
+}
+
+const pageSize = 512 * 1024 * 1024
+
+// Open open a VBF3Redis instance when exists, otherwise create it.
+// When parameters are not match with existing one, this will fail.
+func Open(ctx context.Context, uc redis.UniversalClient, name string, m uint64, k uint, maxLife uint8) (*VBF3Redis, error) {
+	var key = keyBase(name)
+	// FIXME: introduce transaction.
+	p, ok, err := propsGet(ctx, uc, key)
+	if err != nil {
+		return nil, err
+	}
+	props := vbf3props{
+		M:       m,
+		K:       k,
+		MaxLife: maxLife,
+	}
+	if ok && props != *p {
+		return nil, fmt.Errorf("mismatch parameter: want=%+v got=%+v", props, *p)
+	}
+	if !ok {
+		// create/setup a new VBF3 instance on Redis.
+		err := propsPut(ctx, uc, key, &props)
+		if err != nil {
+			return nil, err
+		}
+		err = putGen(ctx, uc, key, &vbf3gen{
+			Bottom: 1,
+			Top:    maxLife,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &VBF3Redis{
+		key:       key,
+		vbf3props: props,
+		c:         uc,
+		pageNum:   int(m/pageSize + 1),
+	}, nil
+}
+
+func (rf *VBF3Redis) hash(d []byte, n uint) int {
+	return int(metro.Hash64(d, uint64(n)) % rf.M)
+}
+
+func (rf *VBF3Redis) Put(ctx context.Context, d []byte, life uint8) error {
+	if life > rf.MaxLife {
+		return fmt.Errorf("life should be less than (<=) %d", rf.MaxLife)
+	}
+	gen, err := getGen(ctx, rf.c, rf.key)
+	if err != nil {
+		return err
+	}
+
+	xx := make([]int, rf.K)
+	readArgs := make([]interface{}, 0, rf.K*3)
+	for i := uint(0); i < rf.K; i++ {
+		x := rf.hash(d, i)
+		xx[i] = x
+		readArgs = append(readArgs, "GET", "u8", x*8)
+	}
+	r, err := rf.c.BitField(ctx, rf.key.data(0), readArgs...).Result()
+	if err != nil {
+		return fmt.Errorf("failed to get data for put (args=%+v): %w", readArgs, err)
+	}
+
+	nv := m255p1add(gen.Bottom, life-1)
+	writeArgs := make([]interface{}, 0, rf.K*3)
+	for i := uint(0); i < rf.K; i++ {
+		v := uint8(r[i])
+		var curr uint8
+		if gen.isValid(v) {
+			curr = v - gen.Bottom + 1
+			if v < gen.Bottom {
+				curr--
+			}
+		}
+		if curr == 0 || life > curr {
+			writeArgs = append(writeArgs, "SET", "u8", xx[i]*8, nv)
+		}
+	}
+	if len(writeArgs) > 0 {
+		_, err := rf.c.BitField(ctx, rf.key.data(0), writeArgs...).Result()
+		if err != nil {
+			return fmt.Errorf("failed to set data for put: %w", err)
+		}
+	}
+	return nil
+}
+
+type vbf3pair struct {
+	x int
+	v uint8
+}
+
+func (rf *VBF3Redis) Check(ctx context.Context, d []byte) (bool, error) {
+	gen, err := getGen(ctx, rf.c, rf.key)
+	if err != nil {
+		return false, err
+	}
+	xx := make([]int, rf.K)
+	args := make([]interface{}, 0, 3*rf.K)
+	for i := uint(0); i < rf.K; i++ {
+		x := rf.hash(d, i)
+		args = append(args, "GET", "u8", x*8)
+		xx[i] = x
+	}
+	vv, err := rf.c.BitField(ctx, rf.key.data(0), args...).Result()
+	if err != nil {
+		return false, fmt.Errorf("failed to check/get data: %w", err)
+	}
+	invalids := make([]vbf3pair, 0, len(vv))
+	retval := true
+	for i, v := range vv {
+		v8 := uint8(v)
+		if !gen.isValid(v8) {
+			retval = false
+			invalids = append(invalids, vbf3pair{x: xx[i], v: v8})
+		}
+	}
+	if len(invalids) >= 0 {
+		args := make([]interface{}, 0, 3*len(invalids))
+		for _, d := range invalids {
+			args = append(args, "SET", "u8", d.x*8, 0)
+		}
+		_, err := rf.c.BitField(ctx, rf.key.data(0), args...).Result()
+		if err != nil {
+			return retval, fmt.Errorf("failed to clear invalids: %w", err)
+		}
+	}
+	return retval, nil
+}
+
+func (rf *VBF3Redis) AdvanceGeneration(ctx context.Context, generations uint8) error {
+	return watchWithRetry(ctx, rf.c, func(tx *redis.Tx) error {
+		gen, err := getGen(tx.Context(), tx, rf.key)
+		if err != nil {
+			return err
+		}
+		gen.Bottom = m255p1add(gen.Bottom, generations)
+		gen.Top = m255p1add(gen.Top, generations)
+		next, err := json.Marshal(gen)
+		if err != nil {
+			return err
+		}
+		_, err = tx.Pipelined(tx.Context(), func(pipe redis.Pipeliner) error {
+			pipe.Set(tx.Context(), rf.key.gen(), next, 0)
+			return nil
+		})
+		return err
+	}, rf.key.gen())
+}
+
+func (rf *VBF3Redis) Sweep(ctx context.Context) error {
+	//defer func(st time.Time) {
+	//	log.Printf("Sweep took %s", time.Since(st))
+	//}(time.Now())
+	gen, err := getGen(ctx, rf.c, rf.key)
+	if err != nil {
+		return err
+	}
+	for pn := 0; pn < rf.pageNum; pn++ {
+		keyData := rf.key.data(pn)
+		err := watchWithRetry(ctx, rf.c, func(tx *redis.Tx) error {
+			b, err := tx.Get(tx.Context(), keyData).Bytes()
+			if err != nil {
+				if errors.Is(err, redis.Nil) {
+					return nil
+				}
+				return err
+			}
+			var modified bool
+			for i, d := range b {
+				if d != 0 && !gen.isValid(d) {
+					b[i] = 0
+					modified = true
+				}
+			}
+			if !modified {
+				return nil
+			}
+			_, err = tx.Pipelined(tx.Context(), func(pipe redis.Pipeliner) error {
+				pipe.Set(tx.Context(), keyData, b, 0)
+				return nil
+			})
+			return err
+		}, keyData)
+		if err != nil {
+			return fmt.Errorf("failed to sweep key:%q: %w", keyData, err)
+		}
+	}
+	return nil
+}
+
+func m255p1add(a, b uint8) uint8 {
+	v := uint16(a) + uint16(b)
+	if v <= 255 {
+		return uint8(v)
+	}
+	return uint8(v - 255)
+}
+
+func (rf *VBF3Redis) Drop(ctx context.Context) error {
+	_, err := rf.c.Pipelined(ctx, func(pipe redis.Pipeliner) error {
+		for i := 0; i < rf.pageNum; i++ {
+			pipe.Del(ctx, rf.key.data(i))
+		}
+		pipe.Del(ctx, rf.key.gen())
+		pipe.Del(ctx, rf.key.props())
+		return nil
+	})
+	return err
+}
---
+++ vbf3redis/vbf3_redis_test.go
@@ -0,0 +1,252 @@
+package vbf3redis
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"strconv"
+	"testing"
+)
+
+func checkVBF3Redis(t *testing.T, m uint64, k uint, num int, f float64) {
+	t.Run(fmt.Sprintf("m=%d k=%d num=%d f=%f", m, k, num, f), func(t *testing.T) {
+		ctx := context.Background()
+
+		mid := int(float64(num) * f)
+		c := newTestRedisClient(t)
+		rf, err := Open(ctx, c, t.Name(), m, k, 10)
+		if err != nil {
+			t.Fatalf("failed to create: %s", err)
+		}
+		t.Cleanup(func() {
+			rf.Drop(ctx)
+		})
+		for i := 0; i < mid; i++ {
+			err := rf.Put(ctx, []byte(strconv.Itoa(i)), 1)
+			if err != nil {
+				t.Fatalf("put failed: %s", err)
+			}
+		}
+
+		falsePositive := 0
+		for i := mid; i < num; i++ {
+			has, err := rf.Check(ctx, []byte(strconv.Itoa(i)))
+			if err != nil {
+				t.Fatalf("check failed: %s", err)
+			}
+			if has {
+				falsePositive++
+			}
+		}
+		errRate := float64(falsePositive) / float64(num) * 100
+		if errRate > 1 {
+			t.Errorf("too big error rate: %.2f%% false_positive=%d m=%d k=%d n=%d f=%f", errRate, falsePositive, m, k, num, f)
+		}
+	})
+}
+
+func TestVBF3RedisBasic(t *testing.T) {
+	checkVBF3Redis(t, 1000, 7, 200, 0.1)
+	checkVBF3Redis(t, 1000, 7, 200, 0.5)
+	checkVBF3Redis(t, 1000, 7, 200, 0.9)
+	checkVBF3Redis(t, 1000, 7, 400, 0.1)
+	checkVBF3Redis(t, 1000, 7, 700, 0.1)
+	checkVBF3Redis(t, 1000, 7, 1000, 0.1)
+}
+
+func TestVBF3RedisLife(t *testing.T) {
+	c := newTestRedisClient(t)
+	ctx := context.Background()
+	rf, err := Open(ctx, c, t.Name(), 100, 7, 10)
+	if err != nil {
+		t.Fatalf("failed to create: %s", err)
+	}
+	t.Cleanup(func() {
+		rf.Drop(ctx)
+	})
+
+	err = rf.Put(ctx, []byte("foo"), 1)
+	if err != nil {
+		t.Fatalf("put \"foo\" failed: %s", err)
+	}
+	err = rf.Put(ctx, []byte("bar"), 2)
+	if err != nil {
+		t.Fatalf("put \"bar\" failed: %s", err)
+	}
+	err = rf.Put(ctx, []byte("baz"), 3)
+	if err != nil {
+		t.Fatalf("put \"baz\" failed: %s", err)
+	}
+
+	for i := 0; i < 2; i++ {
+		for _, tc := range []struct {
+			name string
+			life int
+		}{
+			{"foo", 1},
+			{"bar", 2},
+			{"baz", 3},
+		} {
+			want := tc.life > i
+			got, err := rf.Check(ctx, []byte(tc.name))
+			if err != nil {
+				t.Fatalf("check %q (#%d) failed: %s", tc.name, i, err)
+			}
+			if got != want {
+				t.Errorf("unexpected check %q (%d): want=%t got=%t", tc.name, i, want, got)
+			}
+		}
+		err := rf.AdvanceGeneration(ctx, 1)
+		if err != nil {
+			t.Fatalf("failed to AdvanceGeneration (%d): %s", i, err)
+		}
+	}
+}
+
+func BenchmarkVBF3RedisPut(b *testing.B) {
+	c := newTestRedisClient(b)
+	ctx := context.Background()
+	rf, err := Open(ctx, c, b.Name(), uint64(b.N*10), 7, 10)
+	if err != nil {
+		b.Fatalf("failed to create: %s", err)
+	}
+	b.Cleanup(func() {
+		rf.Drop(ctx)
+	})
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := rf.Put(ctx, []byte(strconv.Itoa(i)), 1)
+		if err != nil {
+			b.Fatalf("put failed: %s", err)
+		}
+	}
+}
+
+func BenchmarkVBF3RedisCheck(b *testing.B) {
+	c := newTestRedisClient(b)
+	ctx := context.Background()
+	rf, err := Open(ctx, c, b.Name(), uint64(b.N*10), 7, 10)
+	if err != nil {
+		b.Fatalf("failed to create: %s", err)
+	}
+	b.Cleanup(func() {
+		rf.Drop(ctx)
+	})
+
+	for i := 0; i < b.N; i++ {
+		rf.Put(ctx, []byte(strconv.Itoa(i)), 1)
+	}
+	max := b.N * 10
+	fail := 0
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		x := int(rand.Int31n(int32(max)))
+		want := x < b.N
+		got, err := rf.Check(ctx, []byte(strconv.Itoa(x)))
+		if err != nil {
+			b.Fatalf("runtime error: %s", err)
+		}
+		if got != want {
+			if !got {
+				b.Fatalf("boom!: N=%d x=%d", b.N, x)
+			}
+			fail++
+		}
+	}
+	rate := float64(fail) / float64(b.N) * 100
+	if rate > 1 && b.N > 100 {
+		b.Logf("too big error rate: %.2f%% failure=%d total=%d", rate, fail, b.N)
+	}
+}
+
+func BenchmarkVBF3RedisAdvanceGeneration(b *testing.B) {
+	c := newTestRedisClient(b)
+	ctx := context.Background()
+	rf, err := Open(ctx, c, b.Name(), 10*1000, 7, 10)
+	if err != nil {
+		b.Fatalf("failed to create: %s", err)
+	}
+	b.Cleanup(func() {
+		rf.Drop(ctx)
+	})
+
+	for i := 0; i < 1000; i++ {
+		rf.Put(ctx, []byte(strconv.Itoa(i)), 128)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err = rf.AdvanceGeneration(ctx, 1)
+		if err != nil {
+			b.Fatalf("runtime error: %s", err)
+		}
+	}
+}
+
+func BenchmarkVBF3RedisSweep(b *testing.B) {
+	c := newTestRedisClient(b)
+	ctx := context.Background()
+	rf, err := Open(ctx, c, b.Name(), uint64(b.N*10), 7, 10)
+	if err != nil {
+		b.Fatalf("failed to create: %s", err)
+	}
+	b.Cleanup(func() {
+		rf.Drop(ctx)
+	})
+
+	buf := make([]byte, b.N*10)
+	rand.Read(buf)
+	_, err = c.Set(ctx, rf.key.data(0), buf, 0).Result()
+	if err != nil {
+		b.Fatalf("failed to setup: %s", err)
+	}
+
+	b.ResetTimer()
+	err = rf.Sweep(ctx)
+	if err != nil {
+		b.Fatalf("runtime error: %s", err)
+	}
+}
+
+func testVBF3RedisTopBottom(ctx context.Context, t *testing.T, rf *VBF3Redis, bottom, top uint8) {
+	t.Helper()
+	gen, err := getGen(ctx, rf.c, rf.key)
+	if err != nil {
+		t.Errorf("faield to get vbf3gen: %s", err)
+		return
+	}
+	if gen.Bottom != bottom {
+		t.Errorf("bottom mismatch: want=%d got=%d", bottom, gen.Bottom)
+	}
+	if gen.Top != top {
+		t.Errorf("top mismatch: want=%d got=%d", top, gen.Top)
+	}
+}
+
+func TestVBF3RedisAdvanceGeneration(t *testing.T) {
+	c := newTestRedisClient(t)
+	ctx := context.Background()
+	rf, err := Open(ctx, c, t.Name(), 256, 1, 1)
+	if err != nil {
+		t.Fatalf("failed to create: %s", err)
+	}
+	t.Cleanup(func() {
+		rf.Drop(ctx)
+	})
+
+	testVBF3RedisTopBottom(ctx, t, rf, 1, 1)
+	if t.Failed() {
+		t.Fatal("failed at first round")
+	}
+
+	for i := 2; i <= 255; i++ {
+		rf.AdvanceGeneration(ctx, 1)
+		want := uint8(i)
+		testVBF3RedisTopBottom(ctx, t, rf, want, want)
+		if t.Failed() {
+			t.Fatalf("failed at round #%d", i)
+		}
+	}
+
+	rf.AdvanceGeneration(ctx, 1)
+	testVBF3RedisTopBottom(ctx, t, rf, 1, 1)
+}
